b1f71d140b7915e69f54d814d6893e88b61770dd
ilyas lokhand <ilokhandwala786@gmail.com>
2025-07-16 15:32:12 +0530

    FINAL

--- src/utils/getCloudinaryPublicId.js
+++ src/utils/getCloudinaryPublicId.js
@@ -10,3 +10,20 @@ function getCloudinaryPublicId(ImageUrl){
 }
 
 export default getCloudinaryPublicId;
+
+
+// example:
+
+// url = https://res.cloudinary.com/demo/image/upload/v1710000000/my-folder/my-image.jpg
+// Is URL me "my-image.jpg" ka public ID hota hai "my-image" (bina extension .jpg ke).
+// ImageUrl.split('/')
+// 👉 Yeh line URL ko / ke base pe todti hai.
+// parts[parts.length - 1]
+// filename.split('.')[0]
+// 👉 Ab filename ko . ke base pe todta hai:
+// "my-image.jpg" => ["my-image", "jpg"]
+// Aur .split('.')[0] matlab "my-image"
+
+// Return karta hai publicId
+// "my-image"
+
